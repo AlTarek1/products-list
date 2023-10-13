892f45f7b03a95c7ef67fea486095e6ea87a93a1
Yousef Tarek <68440801+AlTarek1@users.noreply.github.com>
2023-10-13 09:57:12 +0300

    Add files via upload

--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-            </div>
-            {ListItem}
+  const { items, del, IncreaseQuantity, DecreaseQuantity } = props;
+  let length = items.length;
+  const ListItem = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id} className="item">
+          <p>{item.product}</p>
+          <p>{item.price}</p>
+          <div className="quantity">
+            <span onClick={() => IncreaseQuantity(item.id)}>+</span>
+            <span>{item.quantity}</span>
+            <span onClick={() => DecreaseQuantity(item.id)}>-</span>
+          </div>
+          <p className="delete" onClick={() => del(item.id)}>
+            &times;
+          </p>
         </div>
-    )
-}
+      );
+    })
+  ) : (
+    <div className="text">There are no items, Try to add some.</div>
+  );
+  return (
+    <div>
+      <div className="header item">
+        <p>Product</p>
+        <p>Price</p>
+        <p>Quantity</p>
+        <p>Edit</p>
+      </div>
+      {ListItem}
+    </div>
+  );
+};
 
-export default Items
+export default Items;
